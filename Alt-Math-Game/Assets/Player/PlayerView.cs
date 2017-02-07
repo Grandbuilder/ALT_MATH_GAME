d0e7d69f9790df3b26239a0e30edf930269dfead
hub10 <adambennion17@hotmail.com>
2017-02-06 20:13:21 -0700

    Added player view (camera follows monster prefab).

---
+++ Alt-Math-Game/Assets/Player/PlayerView.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerView : MonoBehaviour {
+    public int lookSpeed = 5;
+    public GameObject enemy;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //look towards target
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(enemy.transform.position - transform.position), lookSpeed * Time.deltaTime);
+    }
+}
