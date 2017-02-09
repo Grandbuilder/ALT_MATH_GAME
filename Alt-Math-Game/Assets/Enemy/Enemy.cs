57de3789049a7413c708ff1d1a80505d43c5a374
Shafigh Bahamin <bahaminshafigh@gmail.com>
2017-02-08 20:11:24 -0700

    did the move towards the player function

--- Alt-Math-Game/Assets/Enemy/Enemy.cs
+++ Alt-Math-Game/Assets/Enemy/Enemy.cs
@@ -2,15 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour {
+public class Enemy : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    Vector3 playerLoc = Camera.main.transform.position;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newVec = this.transform.position;
+        newVec[0] = this.transform.position.x;
+        newVec[1] = this.transform.position.y;
+        newVec[2] = this.transform.position.z;
+        //move towards player
+        if (newVec[0] > playerLoc.x)
+        {
+            newVec[0]--;
+        }
+        else
+        {
+            newVec[0]++;
+        }
+        if (newVec[2] > playerLoc.z)
+        {
+            newVec[2]--;
+        }
+        else
+        {
+            newVec[2]++;
+        }
+        this.transform.position = newVec;
+    }
 }
