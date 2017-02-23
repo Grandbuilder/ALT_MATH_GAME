88370cd598fed2b5ebbfd30906591bd23b9f21a0
hub10 <adambennion17@hotmail.com>
2017-02-22 20:48:28 -0700

    Added Game Menu

---
+++ Alt-Math-Game/Assets/Level/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    int numScenes = 3;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void nextScene()
+    {
+        if(SceneManager.GetActiveScene().buildIndex == numScenes)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        }
+    }
+}
